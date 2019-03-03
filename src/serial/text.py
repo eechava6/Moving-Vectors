d1726022e387bd8e602c0d3f76e93318680ed75a
Esteban <eechava6@eafit.edu.co>
2019-03-03 15:51:31 -0500

    Folder update for testing

--- src/serial/text.py
+++ src/serial/text.py
@@ -9,9 +9,9 @@ def text_analysis():
             if word == "/":
                 return 0
             start = timer()
-            articles = pd.read_csv("articles1.csv",usecols=[1,2,9])
-            articles1 = pd.read_csv("articles2.csv",usecols=[1,2,9])
-            articles2 = pd.read_csv("articles3.csv",usecols=[1,2,9])
+            articles = pd.read_csv("../articles1.csv",usecols=[1,2,9])
+            articles1 = pd.read_csv("../articles2.csv",usecols=[1,2,9])
+            articles2 = pd.read_csv("../articles3.csv",usecols=[1,2,9])
             articlesFinal = pd.concat([articles, articles1, articles2])
             articlesFinal["frec"] = articlesFinal["content"].str.count(word.lower()) + articlesFinal["content"].str.count(word.upper()) + articlesFinal["content"].str.count(word.capitalize())
             articlesFinal = articlesFinal.sort_values(by='frec',ascending=False)
