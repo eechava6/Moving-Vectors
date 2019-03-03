f5539c3023528b518094d790739b624939b1b732
Esteban <eechava6@eafit.edu.co>
2019-03-03 15:39:07 -0500

    Execution time added and fixed method call

--- src/serial/text.py
+++ src/serial/text.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from timeit import default_timer as timer
 
 def text_analysis():
     try:
@@ -7,14 +8,17 @@ def text_analysis():
             word = input("Ingrese la palabra, para salir ingrese \"/\" : " )
             if word == "/":
                 return 0
+            start = timer()
             articles = pd.read_csv("articles1.csv",usecols=[1,2,9])
             articles1 = pd.read_csv("articles2.csv",usecols=[1,2,9])
             articles2 = pd.read_csv("articles3.csv",usecols=[1,2,9])
             articlesFinal = pd.concat([articles, articles1, articles2])
             articlesFinal["frec"] = articlesFinal["content"].str.count(word.lower()) + articlesFinal["content"].str.count(word.upper()) + articlesFinal["content"].str.count(word.capitalize())
             articlesFinal = articlesFinal.sort_values(by='frec',ascending=False)
+            end = timer()
             print(articlesFinal.iloc[0:10,[3,0,1]])
+            print(end - start)
     except:
-        print("ERROR")
-        counting()
+        print("ERROR ")
+        text_analysis()
 text_analysis()
