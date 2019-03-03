f8d0e5c986184e212ba6388315e2a6864ebaeb2e
Esteban <eechava6@eafit.edu.co>
2019-03-03 17:05:55 -0500

    Added openmp Multithreading example (Need Cython)

---
+++ src/openmp/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Build import cythonize
+
+ext_modules = [
+    Extension(
+        "text",
+        ["text.pyx"],
+        extra_compile_args=['-fopenmp'],
+        extra_link_args=['-fopenmp'],
+    )
+]
+
+setup(
+    name='text',
+    ext_modules=cythonize(ext_modules),
+)
