c7fe22cbdb6d89ed9ad49188ee62584263d48027
Esteban <eechava6@eafit.edu.co>
2019-03-05 08:00:25 -0500

    Python pre-proccesing

--- src/python/serial/text.py
+++ src/python/serial/text.py
@@ -2,31 +2,13 @@ import pandas as pd
 from timeit import default_timer as timer
 
 def text_analysis():
-    try:
-        word = ""
-        while(word != "/"):
-            word = input("Ingrese la palabra, para salir ingrese \"/\" : " ).lower()
-            if word == "/":
-                return 0
-            start = timer()
-            articles = pd.read_csv("articles1.csv",usecols=[1,2,9])
-            articles1 = pd.read_csv("articles2.csv",usecols=[1,2,9])
-            articles2 = pd.read_csv("articles3.csv",usecols=[1,2,9])
-            articlesFinal = pd.concat([articles, articles1, articles2])
-            temp_count=[]
-            for word2 in articlesFinal.values.tolist():
-                title = str(word2[1]).lower()
-                news = str(word2[2]).lower()
-                count = title.count(word)+news.count(word)
-                if count > 0:
-                    temp_count.append([count, word2[0], word2[1]])
-            news = sorted(temp_count, key=lambda x: x[0])
-            news.reverse()
-            end = timer()
-            calculationTime = end-start
-            for i in news[:10]:
-                print(i[0], i[1], i[2])
+    try:    
+        articles = pd.read_csv("/opt/datasets/articles1.csv",usecols=[1,2,9])
+        articles1 = pd.read_csv("/opt/datasets/articles2.csv",usecols=[1,2,9])
+        articles2 = pd.read_csv("/opt/datasets/articles3.csv",usecols=[1,2,9])
+        articlesFinal = pd.concat([articles, articles1, articles2])
+        articlesFinal.head(5)
+        articlesFinal.to_csv('aux.csv', sep='\t')
     except Exception as e:
-        print("ERROR "+str(e))
-        text_analysis()
+            print("ERROR "+str(e))
 text_analysis()
