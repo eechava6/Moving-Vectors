aa4ad092603f82b952ef600ba2b515bc6b643889
eperezn <eperezn@eafit.edu.co>
2019-03-08 17:20:36 -0500

    Adding comments

--- src/c++/serial/main.cpp
+++ src/c++/serial/main.cpp
@@ -34,12 +34,12 @@ int main()
   string token, mystring;
   const char delim = ';';
   int i = 0;
-  //bool bandera = true;  
+  //lee el csv ya procesado por python  
   while (getline(fin, line))
     {
       istringstream ss(line);
       Person person;
-      //ss >> person.id; ss.ignore(delim);
+      //extrae las columnas y las guarda en la struct en su correspondiente espacio
       while(token != line){
 	token = line.substr(0,line.find_first_of(delim));
 	line = line.substr(line.find_first_of(delim) + 1);
@@ -51,16 +51,18 @@ int main()
       }
     
     }
-  cout<<persons.size()<<endl;
+  
   int words[persons.size()+1];
   while(cin>>word){
     if(word=="/"){
       return 0;
     }
+    //Cambia la palabra a lower ya que el dataset fue cambiado a minusculas
   transform(word.begin(),word.end(),word.begin(),::tolower);
   cout<<word<<endl;
   string str;
   string str2;
+  //Itero por cada objeto de la struct para poder hacer el count de cada palabra por content y titulo
   for(unsigned int i=0;i<persons.size();i++){
   str = persons[i].content;
   str2 = persons[i].title;
@@ -68,27 +70,22 @@ int main()
   stringstream ss(str);
   stringstream ss2(str2);
   int cont=0;
+  //Verifica en el contet cuantas repeticiones de la palabra encuentra
   while(ss>>str){
     if(word==str)
       cont++;    
   }
+  //Verifica en el title cuantas repeticiones de la palabra encuentra
   while(ss2>>str2){
     if(word==str2){
       cont++;
     }
   }
+  //Almacena en un arreglo paraluego poder hacer el sort
   words[i]=cont;
   }
-// vector<int> myvector (words, words+8);
-//   sort(myvector.begin(), myvector.end(), greater<int>());
-//   std::cout << "myvector contains:";
-//   for (std::vector<int>::iterator it=myvector.begin(); it!=myvector.end(); ++it)
-//     std::cout << ' ' << *it;
-  // for (unsigned int i=0; i< 1; i++){
-  //   cout << persons[i].id<<endl;
-  //   cout << persons[i].title<<endl;
-  //   cout<< persons[i].content<<endl;
-  // }
+  //Busca el elemento mas grande del arreglo, pero dudo del count
+  std::cout << "The largest element is "  << *std::max_element(words,words+7) << '\n';
   }
 }
 
