007564136664a6bf90fad60136b1560449148f18
Esteban <eechava6@eafit.edu.co>
2019-03-09 17:49:47 -0500

    Hot fix ascending

--- src/c++/serial/main.cpp
+++ src/c++/serial/main.cpp
@@ -128,7 +128,7 @@ int main()
   
 
   //Se imprime en orden ascendente los resultados con el articulo.
-  for(int i = 10; i >= 0; i--){
+  for(int i = filtered.size(); i >= filtered.size()-10; i--){
      cout << words[i] <<" times found in : '"<< filtered[indexes[i]].title <<"'"<< endl;
   }
 
