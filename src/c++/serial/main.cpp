c64cf607763d1a619a95eb11860d187312247830
Esteban <eechava6@eafit.edu.co>
2019-03-09 16:39:57 -0500

    Hot fix

--- src/c++/serial/main.cpp
+++ src/c++/serial/main.cpp
@@ -18,6 +18,33 @@ struct Columns {
   string content;
 };
 
+
+void swap(int *xp, int *yp) 
+{
+  //Swaps between 2 elements 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+} 
+  
+void selectionSort(int arr[], int n) 
+{ 
+    int i, j, min_idx; 
+  
+    // One by one move boundary of unsorted subarray 
+    for (i = 0; i < n-1; i++) 
+    { 
+        // Find the minimum element in unsorted array 
+        min_idx = i; 
+        for (j = i+1; j < n; j++) 
+          if (arr[j] < arr[min_idx]) 
+            min_idx = j; 
+  
+        // Swap the found minimum element with the first element 
+        swap(&arr[min_idx], &arr[i]); 
+    } 
+} 
+
 int main()
 {
   ifstream fin("results.csv");
@@ -98,29 +125,3 @@ int main()
   }
 }
 
-
-void swap(int *xp, int *yp) 
-{
-  //Swaps between 2 elements 
-    int temp = *xp; 
-    *xp = *yp; 
-    *yp = temp; 
-} 
-  
-void selectionSort(int arr[], int n) 
-{ 
-    int i, j, min_idx; 
-  
-    // One by one move boundary of unsorted subarray 
-    for (i = 0; i < n-1; i++) 
-    { 
-        // Find the minimum element in unsorted array 
-        min_idx = i; 
-        for (j = i+1; j < n; j++) 
-          if (arr[j] < arr[min_idx]) 
-            min_idx = j; 
-  
-        // Swap the found minimum element with the first element 
-        swap(&arr[min_idx], &arr[i]); 
-    } 
-} 
